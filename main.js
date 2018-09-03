38b71d65bd6d79052104b8cbcfda7287d02a4bba
Ashley DelVentura <ashley@delventura.com>
2018-09-02 22:14:03 -0400

    acceleration

--- main.js
+++ main.js
@@ -20,6 +20,25 @@ function point(x, y, radius, mass, color) {
     this.color = color;
 
     this.update = function(points) {
+        let ax = 0;
+        let ay = 0;
+
+        for (var i = 0; i < points.length; i++) {
+            let other = points[i];
+            if (other === this) {
+                continue;
+            }
+            let sx = this.x - other.x;
+            let sy = this.y - other.y;
+            let distance = Math.sqrt(sx * sx + sy * sy);
+            let distanceSquared = Math.pow(distance, 2);
+
+            ax -= sx / distanceSquared * other.mass;
+            ay -= sy / distanceSquared * other.mass;
+        }
+
+        this.vx += ax;
+        this.vy += ay;
         this.x += this.vx;
         this.y += this.vy;
     };
@@ -35,10 +54,10 @@ function point(x, y, radius, mass, color) {
 
 let points = [];
 
-for(var i = 0; i < 300; i++) {
-    var x = Math.floor(canvas.width * Math.random());
-    var y = Math.floor(canvas.height * Math.random());
-    var p = new point(x, y, 1.5, 3, pointColor);
+for(let i = 0; i < 300; i++) {
+    let x = Math.floor(canvas.width * Math.random());
+    let y = Math.floor(canvas.height * Math.random());
+    let p = new point(x, y, 1.5, 0.5, pointColor);
     points.push(p);
 }
 
@@ -46,8 +65,8 @@ function loop() {
     ctx.fillStyle = canvasColor;
     ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    for (var i = 0; i < points.length; i++) {
-        points[i].update();
+    for (let i = 0; i < points.length; i++) {
+        points[i].update(points);
         points[i].draw(ctx);
     }
     requestAnimationFrame(loop);
